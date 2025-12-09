4697439530ae388adb4abf5937ce2522ac085272
Binny <maxc0766@gmail.com>
2025-12-09 08:38:54 +0100

    Added cors

--- api/lib/app.ts
+++ api/lib/app.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import { config } from './config';
 import Controller from "./interfaces/controller.interface";
 import mongoose from "mongoose";
+const cors = require('cors');
 
 class App {
     public app: express.Application;
@@ -24,6 +25,10 @@ class App {
     private initializeMiddlewares(): void {
         this.app.use(bodyParser.json());
         this.app.use(morgan('dev'));
+        this.app.use(cors({
+            origin: 'http://localhost:5173',
+            credentials: true
+        }));
     }
 
     public listen(): void {
